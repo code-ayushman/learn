0938448d3367bb3fa59a5a6b8404f23afed9e1aa
ayushman <ajha91900@gmail.com>
2024-05-15 20:44:59 +0530

    first

---
+++ main.js
@@ -0,0 +1,42 @@
+
+let cartQuantity = 0;
+document.querySelector("#cart-summery").
+innerText =`your bag has ${cartQuantity} items`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert("hi");
+//let ame=prompt("what is your name?","Guest");
+//document.write(ame);
+
+
+/*let a  = document.getElementById("ba");
+let b = document.getElementById("ab");
+      function add(){
+        let sum = parseInt(a.value) + parseInt(b.value)
+          alert(sum)
+        }*/
+        //let a  = document.getElementByclass("");
+       //document.getElementsByTagName("");
+       
+       //                   type of vareables and its name 
+
+       
+       
+       
+       
